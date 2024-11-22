94ac77e92cf02870bbc6a198f6cfe070cce3f24d
Pedro Silva do Nascimento <phpedrojs@gmail.com>
2024-11-22 18:54:54 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,69 @@
+const navMobile = document.querySelector('#navMobile');
+const navMobileLinks = document.querySelectorAll('#navMobile a');
+const navMobileButton = document.querySelector('.navMobileButton');
+
+navMobileButton.addEventListener('click', () => {
+    if (navMobile.classList.contains('active')) {
+        navMobile.classList.remove('active');
+    } else { 
+        navMobile.classList.add('active');
+    }
+});
+navMobileLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        navMobile.classList.remove('active');
+    });
+});
+
+
+// Botão de voltar
+const botaoVoltar = document.querySelector('#botaoVoltar');
+const navDesktop = document.querySelector("ul#navDesktop");
+
+window.onscroll = () => {
+    const scrollUser = document.documentElement.scrollTop;
+
+    if (scrollUser > 400) {
+        botaoVoltar.classList.add('activeBackButton');
+        navDesktop.classList.add('navAtivo');
+    } else {
+        navDesktop.classList.remove('navAtivo');
+        botaoVoltar.classList.remove('activeBackButton');
+    }
+};
+
+
+// Scroll automático
+function scrollAuto(call) {
+    const area = document.querySelector(call);
+
+    event.preventDefault();
+    area.scrollIntoView({behavior:'smooth'});
+}
+
+
+// ScrollReveal
+window.Effect = ScrollReveal({
+    reset: false
+});
+
+Effect.reveal("#tituloDev", {
+    distance: '200%',
+    origin: 'left',
+    delay: 900
+});
+Effect.reveal(".headerContainer", {
+    duration: 2000
+});
+Effect.reveal(".imagemDesktop", {
+    distance: '100%',
+    origin: 'left'
+});
+Effect.reveal(".imagemMobile", {
+    distance: '100%',
+    delay: 1000 
+});
+Effect.reveal('.blocoRoxo img', {
+    origin: 'left',
+    distance: '200%',
+});
